772e83286a75f43623f356b0d60c7cde0e4dce26
1269473699 <1269473699@qq.com>
2019-11-10 01:11:22 +0800

    完成进入游戏武汉大学的logo淡入淡出效果

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pygame
 import sys
-from title.title import sel_game
+from title.title import LogoPrinter
 
 pygame.init()
-pygame.display.set_caption("海绵宝宝x逆转裁判")
-action = 0
 vInfo = pygame.display.Info()
-size = width, height = vInfo.current_w, vInfo.current_h
-height = int(0.7*height)
-width = int(height*0.75)
-
+size = height = vInfo.current_h
+height = int(0.7 * height)
+width = int(height * 0.75)
 screen = pygame.display.set_mode((width, height))
 
-if action == 0:
-    while True:
-        for event in pygame.event.get():
-            sel_game(event)
-        pygame.display.update()
+if __name__ == '__main__':
+
+    pygame.display.set_caption("海绵宝宝x逆转裁判")
+    action = 0
+
+    lp = LogoPrinter(width, height, screen)
+    if action == 0:
+        lp.display_logo()
 
--- title/title.py
+++ title/title.py
@@ -1,2 +1,35 @@
-def sel_game(event):
-    return
+import pygame
+from time import sleep
+
+class LogoPrinter:
+    def __init__(self, width, height, screen):
+        self.screen = screen
+        self.logo_size = int(0.25 * height)
+        self.whu = pygame.image.load("resources/pics/WHULogo.jpg").convert()
+        self.whu = pygame.transform.scale(self.whu, (self.logo_size, self.logo_size))
+        self.whu_rect = self.whu.get_rect()
+        self.whu_rect.top = 0.125*height
+        self.whu_rect.left = 0.25*height
+        self.fps = 30
+        self.fClock = pygame.time.Clock()
+
+    def display_logo(self):
+        for i in range(0, 256, 8):
+            self.whu.set_alpha(i)
+            #print(self.whu.get_alpha())
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.whu, self.whu_rect)
+            pygame.display.update()
+            self.fClock.tick(self.fps)
+
+        sleep(1)
+
+        for i in range(255, -1, -8):
+            self.whu.set_alpha(i)
+            #print(self.whu.get_alpha())
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.whu, self.whu_rect)
+            pygame.display.update()
+            self.fClock.tick(self.fps)
+
+        #sleep(1)
