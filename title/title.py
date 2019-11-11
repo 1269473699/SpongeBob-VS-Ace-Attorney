975111c5441724d5a9d896bbb9f85b410dbea421
chaos <769489191@qq.com>
2019-11-11 16:40:23 +0800

    规范了命名

--- title/title.py
+++ title/title.py
@@ -39,19 +39,22 @@ class LogoPrinter:
             self.screen.blit(self.member, self.member_rect)
             pygame.display.update()
             self.fClock.tick(self.fps)
+        sleep(1)
+
 
 class TitleInterface:
     def __init__(self,width,height,screen):
         self.screen = screen
-        self.toptitle = pygame.image.load('resources/pics/titleBackground.ver1.jpg')
-        self.bottomtitle = pygame.image.load('resources/pics/UnderTheTitle.jpg')
-        self.toptitle = pygame.transform.scale(self.toptitle,(width,int(0.5*height)))
-        self.bottomtitle = pygame.transform.scale(self.bottomtitle,(width,int(0.5*height)))
-        self.toptitle_rect = self.toptitle.get_rect()
-        self.bottomtitle_rect = self.bottomtitle.get_rect()
-        self.bottomtitle_rect.top = 0.5*height
+        self.top_title = pygame.image.load('resources/pics/titleBackground.ver1.jpg')
+        self.bottom_title = pygame.image.load('resources/pics/UnderTheTitle.jpg')
+        self.top_title = pygame.transform.scale(self.top_title, (width, int(0.5*height)))
+        self.bottom_title = pygame.transform.scale(self.bottom_title,(width,int(0.5*height)))
+        self.top_title_rect = self.top_title.get_rect()
+        self.bottom_title_rect = self.bottom_title.get_rect()
+        self.bottom_title_rect.top = 0.5*height
         self.fps = 30
         self.fClock = pygame.time.Clock()
+
     def display_title(self):
         while True:
             for event in pygame.event.get():
@@ -60,7 +63,7 @@ class TitleInterface:
                         print('nextPart')
 
             self.screen.fill((0, 0, 0))
-            self.screen.blit(self.toptitle, self.toptitle_rect)
-            self.screen.blit(self.bottomtitle, self.bottomtitle_rect)
+            self.screen.blit(self.top_title, self.top_title_rect)
+            self.screen.blit(self.bottom_title, self.bottom_title_rect)
             pygame.display.update()
             self.fClock.tick(self.fps)
