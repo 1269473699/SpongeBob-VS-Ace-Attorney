10629aafebf58fd79be8b67016b16e0f8ff54721
chaos <769489191@qq.com>
2019-11-23 23:05:12 +0800

    增加了可变按钮类

--- util/Button.py
+++ util/Button.py
@@ -45,3 +45,49 @@ class Button:
         else:
             return True
 
+class ChangeableButton(Button):
+    def __init__(self, width, height, screen, surfaces, top, left, hovered_sound, clicked_sound):
+        super().__init__(width, height, screen, surfaces, top, left, hovered_sound, clicked_sound)
+        self.condition = 0
+
+    def display_button(self):
+        if self.condition == 1:
+            self.screen.blit(self.surfaces[self.i], self.button_rect)
+        else:
+            self.screen.blit(self.surfaces[4], self.button_rect)
+
+    def respond_to_clicking(self, event):
+        if self.condition == 1:
+            if event.pos[0] > self.button_rect.left and event.pos[0] < self.button_rect.left + self.button_rect.width and event.pos[1] > self.button_rect.top and event.pos[1] < self.button_rect.top + self.button_rect.height:
+                self.i = 2
+                self.clicked_sound.play()
+                return True
+            return False
+        else:
+            return False
+
+    def respond_to_hovering(self, event):
+        if self.condition == 1:
+            if event.buttons[0] == 0:
+                if event.pos[0] > self.button_rect.left and event.pos[0] < self.button_rect.left + self.button_rect.width and event.pos[1] > self.button_rect.top and event.pos[1] < self.button_rect.top + self.button_rect.height:
+                    self.i = 1
+                    if self.sound_control == 0:
+                        self.hovered_sound.play()
+                    self.sound_control = 1
+                else:
+                    self.i = 0
+                    self.sound_control = 0
+
+    def respond_to_up(self, event):
+        if self.condition == 1:
+            self.i = 0
+            if event.pos[0] > self.button_rect.left and event.pos[0] < self.button_rect.left + self.button_rect.width and event.pos[1] > self.button_rect.top and event.pos[1] < self.button_rect.top + self.button_rect.height:
+                return True
+            else:
+                return True
+        else:
+            return False
+
+    def enable(self, i): #i为bool类型
+        self.condition = i
+
